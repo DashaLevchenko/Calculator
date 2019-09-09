7843ed83635cb6975a0865d2a120786e3eb98636
User <dashulalevchenko@gmail.com>
2019-09-09 17:37:54 +0300

    start controller

--- src/main/java/View/Calculator_Controller.java
+++ src/main/java/View/Calculator_Controller.java
@@ -1,3 +1,97 @@
 package View;
+
+        import java.net.URL;
+        import java.util.ResourceBundle;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+
 public class Calculator_Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button per_cent;
+
+    @FXML
+    private Button Backspace;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button CE;
+
+    @FXML
+    private Button plusMinus;
+
+    @FXML
+    private Button C;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button sum;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    private Button point;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    private Button sqrX;
+
+    @FXML
+    private Button sqrt;
+
+    @FXML
+    private Button OneDivideX;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button difference;
+
+    @FXML
+    private Button divide;
+
+    @FXML
+    private Button multiply;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    void initialize() {
+
+
+    }
 }
