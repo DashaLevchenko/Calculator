19b10fedac2b64bed1cc425a915bd0e146cb85c7
User <dashulalevchenko@gmail.com>
2019-12-11 10:51:56 +0200

    before change controller operations

--- src/main/java/Demo/Demo.java
+++ src/main/java/Demo/Demo.java
@@ -1,40 +1,19 @@
 package Demo;
 
 import Model.Calculator;
-import Model.OperationsEnum;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 
 
 /**
  * This class demonstrations calculator model.
  */
 public class Demo {
-    private static HashMap<String, OperationsEnum> operations = new HashMap<>();
 
-    static {
-        operations.put("+", OperationsEnum.ADD);
-        operations.put("-", OperationsEnum.SUBTRACT);
-        operations.put("/", OperationsEnum.DIVIDE);
-        operations.put("x", OperationsEnum.MULTIPLY);
-
-        operations.put("√", OperationsEnum.SQRT);
-        operations.put("", OperationsEnum.SQR);
-        operations.put("", OperationsEnum.ONE_DIVIDE_X);
-        operations.put("%", OperationsEnum.PERCENT);
-
-        operations.put("", OperationsEnum.NEGATE);
-        operations.put("=", OperationsEnum.EQUAL);
-    }
 
     public static void main (String[] args) {
-
         try {
-            BigDecimal result = Calculator.calculator(5, OperationsEnum.ADD, 3, OperationsEnum.DIVIDE, 2, OperationsEnum.SUBTRACT,
-                    1, OperationsEnum.EQUAL, OperationsEnum.SQRT, OperationsEnum.ADD, 4, OperationsEnum.EQUAL);
-
-
+            BigDecimal result = Calculator.calculator(0,0,0,0,0,0,0);
             System.out.println("Result: " + result);
         } catch (Exception e) {
             e.printStackTrace();
