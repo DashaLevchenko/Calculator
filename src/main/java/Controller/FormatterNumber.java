6839818525c03e1e28cede55e1dbbb9b5f05e4d3
User <dashulalevchenko@gmail.com>
2019-12-18 17:19:29 +0200

    before change formatterNumber 2

--- src/main/java/Controller/FormatterNumber.java
+++ src/main/java/Controller/FormatterNumber.java
@@ -200,36 +200,14 @@ class FormatterNumber {
         int precision = number.precision();
         int numericInNumber = precision - scale;
 
-//
-//        String decimalPattern;
-//        String hashPattern;
-//        String exponentPattern;
-//
-//        int hashRepeat = 0;
-//        if (isMoreMaxScale(numericInNumber)) {
-//            decimalPattern = DECIMAL_PATTERN;
-//
-//            number = number.stripTrailingZeros();
-//            precision = number.precision();
-//            scale = number.scale();
-//
-//            if (precision != PRECISION_MIN && precision > scale) {
-//                hashRepeat = numericInNumber;
-//            }
-//            exponentPattern = EXPONENT_PATTERN;
-//        } else {
-//            if (scale < 0) {
-//                decimalPattern = DECIMAL_PATTERN;
-//                exponentPattern = EXPONENT_PATTERN;
-//            } else {
-//                decimalPattern = INTEGER_PATTERN;
-//                exponentPattern = EMPTY_STRING;
-//            }
-//        }
-//
+
+        String decimalPattern;
+        String exponentPattern;
 
         int hashRepeat = 0;
         if (isMoreMaxScale(numericInNumber)) {
+            decimalPattern = DECIMAL_PATTERN;
+
             number = number.stripTrailingZeros();
             precision = number.precision();
             scale = number.scale();
@@ -237,12 +215,32 @@ class FormatterNumber {
             if (precision != PRECISION_MIN && precision > scale) {
                 hashRepeat = numericInNumber;
             }
+            exponentPattern = EXPONENT_PATTERN;
+        } else {
+            if (scale < 0) {
+                decimalPattern = DECIMAL_PATTERN;
+                exponentPattern = EXPONENT_PATTERN;
+            } else {
+                decimalPattern = INTEGER_PATTERN;
+                exponentPattern = EMPTY_STRING;
+            }
         }
 
 
+//        int hashRepeat = 0;
+//        if (isMoreMaxScale(numericInNumber)) {
+//            number = number.stripTrailingZeros();
+//            precision = number.precision();
+//            scale = number.scale();
+//
+//            if (precision != PRECISION_MIN && precision > scale) {
+//                hashRepeat = numericInNumber;
+//            }
+//        }
+
         String hashPattern = HASH_PATTERN.repeat(hashRepeat);
 
-        String addToPattern = DECIMAL_PATTERN.concat(hashPattern).concat(EXPONENT_PATTERN);
+        String addToPattern = decimalPattern.concat(hashPattern).concat(exponentPattern);
         return pattern.append(addToPattern);
     }
 
